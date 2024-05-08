060b4551c752d7d99c5dcd9cad3955e327376313
RodrigoTdev <rodrigotobiodev@gmail.com>
2024-05-07 23:28:05 -0300

    part 2 - 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,26 +2,29 @@ import { useState } from 'react'
 import { Persons } from './components/Persons'
 import { PersonForm } from './components/PersonForm'
 import { Filter } from './components/Filter'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data)
+      console.log('response.data:', response.data)
+    })
+  }, [])
+
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(search.toLowerCase())
   )
-  console.log(filteredPersons)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!persons.some((person) => person.name === newName)) {
-      console.log('only false')
       setPersons((prev) => prev.concat({ name: newName, number: newNumber }))
     } else {
       alert(`${newName} is already added to phonebook`)
@@ -40,7 +43,6 @@ const App = () => {
       />
       <h3>Numbers</h3>
       <Persons persons={filteredPersons} />
-      {console.log(search)}
     </div>
   )
 }
