a7d630b80e74c924e3778c5daa7d4494e5a8fc1b
RodrigoTdev <rodrigotobiodev@gmail.com>
2024-05-12 00:53:14 -0300

    part 3 - backend full with express static files

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -27,6 +27,7 @@ let db = [
 ]
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'))
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body ')
