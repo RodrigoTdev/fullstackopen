0a9d20b453ca33d70cfdd4a73681f4e5c2e39f54
RodrigoTdev <rodrigotobiodev@gmail.com>
2024-05-13 20:15:52 -0300

    part4 blogs

---
+++ part4/blog/README.md
@@ -0,0 +1,3 @@
+# Blog list
+
+## Falta conectar a MongoDB
---
+++ part4/blog/backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  try {
+    Blog.find({}).then((blogs) => {
+      response.json(blogs)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+  try {
+    blog.save().then((result) => {
+      response.status(201).json(result)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
